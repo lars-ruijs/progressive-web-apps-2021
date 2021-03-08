50b7a2e0b1cb4c466a8edb6b4149445b3898c1cc
lars-ruijs <60745347+lars-ruijs@users.noreply.github.com>
2021-03-08 17:20:32 +0100

    Astronomy overview page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getData } from './modules/helpers.js';
 
+// Create a express app
 const app = express();
 
 // listen for request
@@ -8,6 +9,10 @@ app.listen(3000);
 
 // Views engine
 app.set('view engine', 'ejs');
+
+// Set static file directory
+// Source: https://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
 
 const astronomyBase = "planetary/apod";
 const roverBase = "mars-photos/api/v1/rovers/";
