3d578782e5997671a8072ad490c96669789d42b3
lars-ruijs <60745347+lars-ruijs@users.noreply.github.com>
2021-03-16 12:08:35 +0100

    Split routes into module

--- app.js
+++ app.js
@@ -1,38 +1,16 @@
 // Import dotenv with ES6 modules via https://github.com/motdotla/dotenv/issues/89#issuecomment-587753552
 import 'dotenv/config.js';
 import express from 'express';
-import { homeRoute } from './render/overview.js';
-import { astroDetailRoute, roverDetailRoute } from './render/detail.js';
+import { router } from './routes/router.js';
 
 // Create a express app
 const app = express();
 const port = process.env.PORT || 3000;
 
-// listen for request
-app.listen(port);
-
-// Views engine
-app.set('view engine', 'ejs');
-
 // Set static file directory
 // Source: https://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// Home page (overview)
-app.get('/', homeRoute);
-
-// Detail page astronomy
-app.get('/astronomy/:date', astroDetailRoute);
-
-// Detail page rover
-app.get('/rover/:name/:sol', roverDetailRoute);
-
-app.get('/offline', (req, res) => {
-    res.render('offline', { title: "Offline" });
-});
-
-// 404 page
-// Source: https://expressjs.com/en/starter/faq.html
-app.use((req, res) => {
-    res.status(404).render('404', { title: "404 Not Found" });
-});
+app
+    .set('view engine', 'ejs')
+    .use(express.static('public'))
+    .use('/', router)
+    .listen(port);
