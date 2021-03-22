0b3a3b3df4c90fdfc63a96339070b8d3244c2241
lars-ruijs <60745347+lars-ruijs@users.noreply.github.com>
2021-03-22 16:41:35 +0100

    Add compression for GZIP

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Import dotenv with ES6 modules via https://github.com/motdotla/dotenv/issues/89#issuecomment-587753552
 import 'dotenv/config.js';
+import compression from 'compression';
 import express from 'express';
 import { router } from './routes/router.js';
 
@@ -9,8 +10,10 @@ const port = process.env.PORT || 3000;
 
 // Set static file directory
 // Source: https://expressjs.com/en/starter/static-files.html
+// Use Compression to enable GZIP compression
 app
     .set('view engine', 'ejs')
+    .use(compression())
     .use(express.static('dist'))
     .use('/', router)
     .listen(port);
