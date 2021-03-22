3a22af50dbeb6e7f1beac234d1327a09f687040e
lars-ruijs <60745347+lars-ruijs@users.noreply.github.com>
2021-03-22 16:04:11 +0100

    Add build scripts

---
+++ scripts/build-css.js
@@ -0,0 +1,18 @@
+// Special thanks to Jordy for helping me with this!
+
+import gulp from 'gulp';
+import concat from 'gulp-concat';
+import autoprefixer from 'gulp-autoprefixer';
+import cleanCSS from 'gulp-clean-css';
+
+(function() {
+ return gulp.src('./public/css/styles.css')
+  .pipe(concat('css/styles.css'))
+  .pipe(cleanCSS())
+  .pipe(
+    autoprefixer({
+      cascade: false,
+    })
+  )
+  .pipe(gulp.dest('./dist/'));
+})();
